c4085a79a5fa8323f9efc11f8a7a5c5570a11f87
Tange <tantantatange@gmail.com>
2020-05-20 22:50:22 +0900

    finished 1-3

---
+++ chapter1/Operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int i = 0;
+    // i *= 2 + 4;
+    // i %= 3;
+    i = i * (2 + 4) % 3;
+    std::cout << 1 + 2 * 3 / 2 << std::endl;
+    std::cout << i << std::endl;
+}
