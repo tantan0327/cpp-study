656fd1655e2acc943400af15506fa87f03907606
Tange <tantantatange@gmail.com>
2020-07-04 00:11:43 +0900

    finished 2-2

--- chapter2/Product.cpp
+++ chapter2/Product.cpp
@@ -1,27 +1,83 @@
 #include <iostream>
 
-struct product
+class product
 {
     int id;
     int price;
     int stock;
+
+public:
+    int get_id();
+    void set_id(int new_id);
+
+    int get_price();
+    void set_price(int new_price);
+
+    int get_stock();
+    void set_stock(int new_stock);
+
+    void set(int new_id, int new_price, int new_stock);
 };
 
-void show_product(product product)
+int product::get_id()
 {
-    std::cout << "商品ID: " << (&product)->id << std::endl;
-    std::cout << "単価: " << (&product)->price << std::endl;
-    std::cout << "単価: " << (&product)->stock << std::endl;
+    return id;
+}
+
+void product::set_id(int new_id)
+{
+    id = new_id;
+}
+
+int product::get_price()
+{
+    return price;
+}
+
+void product::set_price(int new_price)
+{
+    if (new_price < 0)
+    {
+        std::cout << "エラー：単価は0以上にしてください" << std::endl;
+        return;
+    }
+    price = new_price;
+}
+
+int product::get_stock()
+{
+    return stock;
+}
+
+void product::set_stock(int new_stock)
+{
+    if (new_stock < 0)
+    {
+        std::cout << "エラー：" << std::endl;
+        return;
+    }
+    stock = new_stock;
+}
+
+void product::set(int new_id, int new_price, int new_stock)
+{
+    id = new_id;
+    price = new_price;
+    stock = new_stock;
 }
 
 int main()
 {
-    product pen = 
-    {
-        0,
-        100,
-        200
-    };
+    product pen;
 
-    show_product(pen);
+    // pen.set_id(0);
+    // pen.set_price(100);
+    // pen.set_stock(200);
+    pen.set(0, 100, 200);
+
+    product *ptr = &pen;
+    std::cout << "商品ID: " << ptr -> get_id() << std::endl;
+    std::cout << "単価: " << ptr -> get_price() << std::endl;
+    std::cout << "在庫数: " << ptr -> get_stock() << std::endl;
+
 }
