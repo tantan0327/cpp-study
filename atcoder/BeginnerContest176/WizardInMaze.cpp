5d5efa3c580e33fa33ceff0df3b3451d09d32394
Tange <tantantatange@gmail.com>
2020-08-29 23:08:54 +0900

    Beginner Contest 176-D

--- atcoder/BeginnerContest176/WizardInMaze.cpp
+++ atcoder/BeginnerContest176/WizardInMaze.cpp
@@ -1,63 +1,61 @@
+// BFS
 #include <bits/stdc++.h>
 #include <string>
 #include <cmath>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 
 using namespace std;
+using P = pair<int, int>;
 
-struct Corr {
-    ll y;
-    ll x;
-    ll depth;
-};
-
-ll H, W;
-ll Ch, Cw;
-ll Dh, Dw;
-ll y, x, depth;
-vector<string> vec;
-queue<Corr> q;
-
-
-vector<ll> y_vec = {0, 0, -1, +1};  // 進む方向の簡便化，テクニックとして覚えておく．
-vector<ll> x_vec = {-1, +1, 0, 0};  // ループで回すことで上下左右に移動できるようになっている
-
-
-ll bfs() {
-    while (!q.empty()) {
-        Corr now = q.front(); q.pop();  // キューの先頭を取り出す
-        y = now.y, x = now.x, depth = now.depth;  // 行，列が y, x になっているので注意
-
-        // 停止条件：現在の座標がゴールなら深さを返す
-        if (y == Dh && x == Dw) return depth;
-
-        // 上下左右 4 方向を探索．進むことができればキューに格納する．
-        for (ll i = 0; i < 4; ++i) {
-            Corr next = {y + y_vec[i], x + x_vec[i], depth + 1};
-            if (0 <= next.y && next.y <= H-1 && 0 <= next.x && next.x <= W-1 && vec[next.y][next.x] == '.') {
-                vec[next.y][next.x] = 'x';  // 同じ場所を探索しないようにする
-                q.push(next);
-            }
-        }
-    }
-    return 0;
-}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, -1, 0, 1};
 
 int main()
 {
     // input
-    cin >> H >> W;
-    cin >> Ch >> Cw;
-    cin >> Dh >> Dw;
-    Ch--, Cw--, Dh--, Dw--;
-    char S[H][W];
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < H; i++) {
-            cin >> S[i][j];
+    int h, w;
+    cin >> h >> w;
+    int si, sj;
+    cin >> si >> sj;
+    int ti, tj;
+    cin >> ti >> tj;
+    --si; --sj; --ti; --tj;
+    vector<string> s(h);
+    rep(i, h) cin >> s[i];
+    const int INF = 1e9;
+    vector<vector<int>> dist(h, vector<int>(w, INF));
+    deque<P> q;
+    dist[si][sj] = 0;
+    q.emplace_back(si, sj);
+    while (!q.empty()) {
+        int i = q.front().first;
+        int j = q.front().second;
+        int d = dist[i][j];
+        q.pop_front();
+        if (dist[i][j] != d) continue;
+        rep(v, 4) {
+            int ni = i+di[v], nj = j+dj[v];
+            if (ni < 0 || ni >= h || nj < 0 || nj >= w) continue;
+            if (s[ni][nj] == '#') continue;
+            if (dist[ni][nj] <= d) continue;
+            dist[ni][nj] = d;
+            q.emplace_front(ni, nj);
+        }
+        for (int ei = -2; ei <= 2; ++ei) {
+            for (int ej = -2; ej <= 2; ++ej) {
+                int ni = i+ei, nj = j+ej;
+                if (ni < 0 || ni >= h || nj < 0 || nj >= w) continue;
+                if (s[ni][nj] == '#') continue;
+                if (dist[ni][nj] <= d+1) continue;
+                dist[ni][nj] = d+1;
+                q.emplace_back(ni, nj);
+            }
         }
     }
-
-    Corr start = {Ch, Cw, 0};  // 初期位置の深さ(depth)は 0
-    q.emplace(start);  // 初期位置をキューに格納
+    int ans = dist[ti][tj];
+    if (ans == INF) ans = -1;
+    cout << ans << endl;
+    return 0;
 
 }
