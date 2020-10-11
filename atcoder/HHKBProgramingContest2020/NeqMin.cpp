a67155d86c6d7fc183373f57456a97e5837b219d
Tange <tantantatange@gmail.com>
2020-10-11 20:53:41 +0900

    Fixed NeqMin

--- atcoder/HHKBProgramingContest2020/NeqMin.cpp
+++ atcoder/HHKBProgramingContest2020/NeqMin.cpp
@@ -2,33 +2,15 @@
 using namespace std;
  
 int main() {
-    int N;
-    cin >> N;
-    int p[N];
-    for (int i = 0; i < N ; i++) {
-        cin >> p[i];
-    }
-    int max_p = 0;
-    int min_p = 2000000;
-    int ans = 0;
-
-    int result = 0;
-
-    for (int i = 0; i < N; i++) {
-        if (min_p > p[i]) {
-            min_p = p[i];
+    int a;scanf("%d",&a);
+    int at=0;
+    for(int i=0;i<a;i++){
+        int s;scanf("%d",&s);
+        t[s]++;
+        while(t[at]){
+            at++;
         }
-        if (result < p[i]) {
-            cout << result << endl;
-        } else {
-            for (int j = 0; j < i+1 ; j++) {
-                if (min_p+1 == p[j]) {
-                    min_p = p[j]+1;
-                }
-            }
-            result = ans;
-            cout << result << endl;
-        }
+        printf("%d\n",at);
     }
  
 }
