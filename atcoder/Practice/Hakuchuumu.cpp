db4ae3a3096ca3f5f0c8577d81b86483d23a83a8
Tange <tantantatange@gmail.com>
2020-10-19 23:06:41 +0900

    Studied Hakuchuumu

---
+++ atcoder/Practice/Hakuchuumu.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+using ll = long long;
+
+string divide[4] = {"dream", "dreamer", "erase", "eraser"};
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    reverse(S.begin(), S.end());
+    for (int i = 0; i < 4; ++i) reverse(divide[i].begin(), divide[i].end());
+
+    bool can = true;
+    for (int i = 0; i < S.size();) {
+        bool can2 = false;
+        for (int j = 0; j < 4; ++j) {
+            string d = divide[j];
+            if (S.substr(i, d.size()) == d) {
+                can2 = true;
+                i += d.size();
+            }
+        }
+        if (!can2) {
+            can = false;
+            break;
+        }       
+    }
+    
+    if (can) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+}
