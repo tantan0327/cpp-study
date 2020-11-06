cd510263cf77475086812cf48f57e0b33257fa4a
Tange <tantantatange@gmail.com>
2020-11-07 00:23:48 +0900

    studied TakahashiUnevolved

---
+++ atcoder/Practice/Brown/TakahashiUnevolved.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+using ll = long long;
+
+ll x, y, a, b, s;
+
+int main()
+{
+    cin >> x >> y >> a >> b;
+    ll ans=0;
+    while((double) a*x<=2e18 && a*x<=x+b && a*x<y) {
+        x*=a;
+        ans++;
+    }
+    cout << ans+(y-1-x)/b << endl;
+}
