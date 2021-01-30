048f5e301a1a05e627dd035b4bdc2d5125b0864b
Tange <tantantatange@gmail.com>
2021-01-30 20:18:26 +0900

    fixes Logical Expression

--- atcoder/abc/abc189/D.cpp
+++ atcoder/abc/abc189/D.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <string>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using ll = long long;
 using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    string S[N];
-    for (int i = 0; i < N; i++) {
-        cin >> S[i];
-    }
+    int n;
+    cin >> n;
+    string S[n];
+    vector<ll> dp(2, 1);
 
-    int result = 0;
-
-    for (int i = N-1; i >= 0; i--) {
-        if (S[i] == "OR") { 
-            result += pow(2, i+1);
+    rep(i, n) {
+        string s;
+        cin >> s;
+        vector<ll> p(2); swap(dp, p);
+        rep(j, 2)rep(x,2) {
+            int nj = j;
+            if (s == "AND") nj &= x;
+            else nj |= x;
+            dp[nj] += p[j];
         }
-        if (S[i] == "AND") {
-            continue;
-        }   
     }
 
-    cout << result+1 << endl;
+    cout << dp[1] << endl;
 
 }
 
