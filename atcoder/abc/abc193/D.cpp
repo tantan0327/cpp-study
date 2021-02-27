d40c3f3f09fd5900e32d890678b2c177d323cbc0
Tange <tantantatange@gmail.com>
2021-02-28 00:16:01 +0900

    fixed Poker

--- atcoder/abc/abc193/D.cpp
+++ atcoder/abc/abc193/D.cpp
@@ -6,65 +6,29 @@
 using ll = long long;
 using namespace std;
 
-int ctoi(const char c){
-    if('0' <= c && c <= '9') return (c-'0');
-    return -1;
+ll score(string S) {
+    vector<ll> cnt(10);
+    iota(cnt.begin(), cnt.end(), 0);
+    for (char c : S) cnt[c - '0'] *= 10;
+    return accumulate(cnt.begin(), cnt.end(), 0);
 }
 
 int main()
 {
     int K;
-    cin >> K;
-    string S;
-    string T;
-    cin >> S >> T;
-    int A[10] = {0};
-    int B[10] = {0};
-    int C[10] = {K};
-    long result = 0;
-    for (int i = 0; i <  (int) S.size()-1; i++) {
-        A[ctoi(S[i])]++;
-        C[ctoi(S[i])]--;
-        B[ctoi(T[i])]++;
-        C[ctoi(T[i])]--;
+    string S, T;
+    cin >> K >> S >> T;
+    vector<ll> cnt(10, K);
+    for (char c : S + T) cnt[c - '0']--;
+    ll win = 0;
+    for (ll x = 1; x<=9; x++) for (ll y = 1; y<=9; y++) {
+        S.back() = '0' + x;
+        T.back() = '0' + y;
+        if (score(S) <= score(T)) continue;
+        win += cnt[x] * (cnt[y] - (x == y));
     }
 
-    int S_score = 0;
-    int T_score = 0;
-
-    // for (int i = 1; i <  10; i++) {
-    //     S_score += pow(10, A[i]) * i;
-    //     T_score += pow(10, A[i]) * i;
-    // }
-
-    for (int i = 1; i < 10; i++) {
-        S_score = 0;
-        T_score = 0;
-        if (S[i] + T[i] == C[i]) {
-            continue;
-        }
-        S[i]++;
-        C[i]--;
-        for (int j = 1; j < 10; j++) {
-            if (S[j] + T[j] == C[j]) {
-                continue;
-            }
-            T[j]++;
-            C[j]--;
-            for (int i = 1; i <  10; i++) {
-                S_score += pow(10, A[i]) * i;
-                T_score += pow(10, A[i]) * i;
-            }
-            if (S_score > T_score) {
-                result += (C[i] + 1) * (C[j] + 1);
-            }
-            T[j]--;
-            C[j]++;
-        }
-        S[i]--;
-        C[i]++;
-    }
-
-    cout << (double) result / (double) ((9 * K - 10) * (9 * K - 11)) << endl;
+    const ll rem = 9 * K - 8;
+    cout << double(win) / rem / (rem - 1) << endl;
 
 }
